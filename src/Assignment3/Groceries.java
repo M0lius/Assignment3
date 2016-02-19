57c97a1295e2229d6a995b13593721c6a9b2ee6e
Ethan Miller <Ethan.mill@utexas.edu>
2016-02-19 15:13:32 -0600

    Updated getters setters superclass and object classes

---
+++ src/Assignment3/Groceries.java
@@ -0,0 +1,10 @@
+package Assignment3;
+
+public class Groceries extends Item {
+
+	public Groceries(String name, double price, double weight, int quantity) {
+		super(name, price, weight, quantity);
+		// TODO Auto-generated constructor stub
+	}
+
+}
